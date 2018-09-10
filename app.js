75c9620cae03632858c2cea97c2776c5eb43b4b7
Hunt Applegate <huntapplegate@gmail.com>
2018-09-10 16:09:53 -0600

    added C to F

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-//rainbow brackets
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    //console.log("DOM fully loaded and parsed");
     var earth = document.querySelector('#earth')
     var search = document.querySelector('#search')
     var body = document.querySelector('body')
@@ -9,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var response = document.querySelector('#response')
     var alert = document.querySelector('#alert')
     var which = document.querySelector('#which')
-    var five = document.querySelector('.btn')
-    var toggle = document.querySelector('#toggle')
+    var five = document.querySelector('#five')
+    var cF = document.querySelector('#cF')
 
     var title = document.querySelectorAll('.card-title')
     var date = document.querySelectorAll('.date')
@@ -41,51 +40,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
       axios.get(`https://cors-anywhere.herokuapp.com/http://www.metaweather.com/api/location/search/?query=${city.value}`).then(result => {
-        if (result.data.length === 0) {
+        var city_data = result.data
+        if (city_data.length === 0) {
           alert.className = 'alert alert-danger'
           alert.innerHTML = `Can't Find Your City!`
-
           setTimeout(function(){alert.innerHTML =''; alert.className =''}, 3000)
+
           for (let i=0; i < card.length; i++) {
             card[i].className = 'card text-center mx-auto invisible'
           }
+
         } else {
-          for (let i = result.data.length -1; i >= 0; i--) {
-            if (result.data[i].title === city.value) {
+          for (let i = city_data.length -1; i >= 0; i--) {
+            if (city_data[i].title === city.value) {
+              var id = city_data[i].woeid
               localStorage.setItem('fav', `${city.value}`)
+              earth.remove()
               alert.innerHTML = `Found Your City!`
               alert.className = 'alert alert-success'
               setTimeout(function(){alert.innerHTML =''; alert.className =''}, 3000)
-              earth.remove()
-              toggle.className = 'btn btn-primary'
-              var id = result.data[i].woeid
+
               axios.get(`https://pixabay.com/api/?key=9836168-009d64875ade45aed31f53b02&q=${city.value}&image_type=photo`).then(result => {
-                var maxhit = result.data.hits.length
-                console.log(result.data.hits)
+                var city_pic = result.data
+                var maxhit = city_pic.hits.length
                 var rand = getRandom(0, maxhit)
-                console.log(result.data.hits[rand].largeImageURL)
-                body.style.backgroundImage = `url('${result.data.hits[rand].largeImageURL}')`
+                body.style.backgroundImage = `url('${city_pic.hits[rand].largeImageURL}')`
+                cF.className = 'btn btn-primary'
               })
               axios.get(`https://cors-anywhere.herokuapp.com/http://www.metaweather.com/api/location/${id}`).then(result => {
-                for (let j=0; j < result.data.consolidated_weather.length; j++) {
-                  //console.log(result.data.consolidated_weather[j])
-                  date[j].textContent = result.data.consolidated_weather[j].applicable_date
+                var weatherC = result.data.consolidated_weather
+                for (let j=0; j < card.length; j++) {
+                  date[j].textContent = weatherC[j].applicable_date
                   title[j].textContent = city.value
-                  temp[j].innerHTML = `${Math.round(result.data.consolidated_weather[j].the_temp)} ℃`
-                  low[j].innerHTML= `${Math.round(result.data.consolidated_weather[j].min_temp)} ℃`
-                  high[j].innerHTML = ` - ${Math.round(result.data.consolidated_weather[j].max_temp)} ℃`
-                  humidity[j].innerHTML = `${result.data.consolidated_weather[j].humidity} %`
-                  speed[j].innerHTML = `${Math.round(result.data.consolidated_weather[j].wind_speed)} mph`
-                  direction[j].innerHTML = ` ${result.data.consolidated_weather[j].wind_direction_compass}`
+                  temp[j].innerHTML = `${Math.round(weatherC[j].the_temp)} ℃`
+                  low[j].innerHTML= `${Math.round(weatherC[j].min_temp)} ℃`
+                  high[j].innerHTML = ` - ${Math.round(weatherC[j].max_temp)} ℃`
+                  humidity[j].innerHTML = `${weatherC[j].humidity} %`
+                  speed[j].innerHTML = `${Math.round(weatherC[j].wind_speed)} mph`
+                  direction[j].innerHTML = ` ${weatherC[j].wind_direction_compass}`
+                  card_img[j].src = `http://www.metaweather.com/static/img/weather/${weatherC[i].weather_state_abbr}.svg`
                   card[0].className = 'card text-center mx-auto'
-                  card_img[j].src = `http://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[i].weather_state_abbr}.svg`
                 }
               })
             } else {
               alert.className = 'alert alert-warning'
               which.className = 'alert alert-danger'
-              alert.innerHTML += `| ${result.data[i].title} |`
-              if (result.data.length === 1) {
+              alert.innerHTML += `| ${city_data[i].title} |`
+              if (city_data.length === 1) {
                 which.innerHTML = 'Did you mean?'
               } else {
                 which.innerHTML = 'Which one?'
@@ -95,20 +96,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
               }
         } //for
       } //else
-    }) //1st get
-}) //click eventlistener
+      cF.addEventListener('click', function(event) {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://www.metaweather.com/api/location/${id}`).then(result => {
+          var weather = result.data.consolidated_weather
+          if (cF.classList.contains('active')) {
+          for (let i=0; i < card.length; i++) {
+            temp[i].innerHTML = `${Math.round((weather[i].the_temp) * 1.8 + 32)} ℉`
+            low[i].innerHTML= `${Math.round((weather[i].min_temp) * 1.8 + 32)} ℉`
+            high[i].innerHTML = ` - ${Math.round((weather[i].max_temp) * 1.8 + 32)} ℉`
+          }
+        } else {
+          for (let i=0; i < card.length; i++) {
+            temp[i].innerHTML = `${Math.round(weather[i].the_temp)} ℃`
+            low[i].innerHTML= `${Math.round(weather[i].min_temp)} ℃`
+            high[i].innerHTML = ` - ${Math.round(weather[i].max_temp)} ℃`
+          }
+        }
+      })
+
+    })// cF listener
+    }) // 1st get
+}) // search listener
+
 five.addEventListener('click', function(event) {
-    for (let i=1; i < card.length; i++) {
+  if (five.classList.contains('active')) {
+    for (let i=0; i < card.length; i++) {
       card[i].className = 'card text-center mx-auto'
     }
+  } else {
+    for (let i=1; i < card.length; i++) {
+      card[i].className = 'card text-center mx-auto invisible'
+    }
+  }
 
-}) //eventlistener
-// toggle.addEventListener('click', function(event) {
-//   if (f.className.contains('active')) {
-//     for (let i=0; i < card.length; i++) {
-//       temp.innerHTML = temp.innerHTML * 1.8 + 32
-//     }
-//   }
-// })
+}) // five list
+
 
 }); //dom
